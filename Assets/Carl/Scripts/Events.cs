6ca9f587842417376e34c2c7f491436f7c335591
GrimRot <damienmojzak@gmail.com>
2022-06-01 22:02:07 -0400

    UI Events

--- Assets/Carl/Scripts/Events.cs
+++ Assets/Carl/Scripts/Events.cs
@@ -6,4 +6,24 @@ public static class Events
     
     public delegate void ExecuteEvent(string eventId);
     public static ExecuteEvent OnExecuteEvent;
+
+    #region UI Events
+    public delegate void BloomChange(float value);
+    public static BloomChange OnBloomChange;
+
+    public delegate void LookSensitivityChange(float value);
+    public static LookSensitivityChange OnLookSensitivityChange;
+
+    public delegate void InvertedControlsChange(bool state);
+    public static InvertedControlsChange OnInvertedControlsChange;
+
+    public delegate void CameraShakechange(bool state);
+    public static CameraShakechange OnCameraShakeChange;
+
+    public delegate void FOVChange(float value);
+    public static FOVChange OnFOVChange;
+
+    public delegate void HoldProgress(float progress);
+    public static HoldProgress OnHoldProgress;
+    #endregion
 }
